fd29bd1dec7a16bba9f9672b2019eb298b624d3b
miyazawa220618 <m.yukika341@gmail.com>
2022-09-28 16:56:03 +0900

    userテーブルの作成
    ・ニックネームと役職、プロフィールテキストのカラムを追加してマイグレート作成
    ・役職をActive Hashにする
    ・プロフィール画像を登録できるように、Active Storageを導入する
    ・ルーティングを設定する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :profile_image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :work
+
+  VALID_PASS_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :work_id,  numericality: {other_than: 1 , message: "can't be blank"}
+    validates :password, format: { with: VALID_PASS_REGEX }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
+
 end
