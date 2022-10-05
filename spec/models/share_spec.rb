5e4395b52b5eb41b370b299ed9528fda55948487
miyazawa220618 <m.yukika341@gmail.com>
2022-10-05 20:08:41 +0900

    モデル単体テストの実行/微調整
    ・単体モデルのFactoryBotの設定
    ・テスト内容の記述・実行
    ・ビューの調整

--- spec/models/share_spec.rb
+++ spec/models/share_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Share, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @share = FactoryBot.build(:share)
+  end
+
+  describe '作業分担メモの新規投稿' do
+    context '新規投稿できる場合' do
+      it '全ての項目が適切に入力されていれば投稿できる' do
+        expect(@share).to be_valid
+      end
+      it '作業メモが空でも投稿できる' do
+        @share.memo = ''
+        expect(@share).to be_valid
+      end
+      it '添付が無くても投稿できる' do
+        @share.memo_files = nil
+        expect(@share).to be_valid
+      end
+    end
+
+    context '新規投稿できない場合' do
+      it 'share_dateが未選択の場合は投稿できない' do
+        @share.share_date = ''
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Share date can't be blank")
+      end
+      it 'hour_idが未選択の場合は投稿できない' do
+        @share.hour_id = 1
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Hour can't be blank")
+      end
+      it 'scheduleと紐づいてなければ投稿できない' do
+        @share.schedule = nil
+        @share.valid?
+        expect(@share.errors.full_messages).to include("Schedule must exist")
+      end
+      it 'userと紐づいてなければ投稿できない' do
+        @share.user = nil
+        @share.valid?
+        expect(@share.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
