dc88658a137bac85ec1cf627955ba81becc58f64
miyazawa220618 <m.yukika341@gmail.com>
2022-10-09 16:17:08 +0900

    自動デプロイの準備
    Capistranoの導入

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+lock '3.17.1'
+
+set :application, 'team_schedule'
+
+set :repo_url,  'https://github.com/miyazawa220618/team_schedule.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5'
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/techcamp.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
